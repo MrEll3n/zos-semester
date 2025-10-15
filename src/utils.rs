540b0f4f774a5911f3bc783af9e3ecbec506ff31
MrEllen_ <mail@mrellen.eu>
2025-10-15 16:10:21 +0200

    refactor: tui moved into utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,2 @@
 pub mod arg_man;
+pub mod tui;
---
+++ src/utils/tui.rs
@@ -0,0 +1,10 @@
+
+pub fn handle_app_loop() {
+    // loop {
+    //
+    // }
+}
+
+pub fn write_greet() {
+    println!("MrEll3n's File System - {}\n", env!("CARGO_PKG_VERSION"));
+}
