06a0291a98f25d2d62e4a8d3e2d7ebe1bee07fa5
MrEllen_ <mail@mrellen.eu>
2025-11-01 21:22:29 +0100

    refactor: minor refactoring
    - added comments
    - moved stdin init on the start of the handle_app_loop()

--- src/utils/tui.rs
+++ src/utils/tui.rs
@@ -11,27 +11,29 @@ fn write_prefix() {
 }
 
 pub fn handle_app_loop() {
+    let stdin = io::stdin();
     let registry = Registry::new();
     let mut context = Context {};
 
-
     write_greet();
-    let stdin = io::stdin();
     loop {
+        // before input
         write_prefix();
         let mut user_input = String::new();
-        let _ = stdin.read_line(&mut user_input);
-        let trimmed = user_input.trim();
 
+        // user input
+        let _ = stdin.read_line(&mut user_input);
+        
+        // preparing user input to be formated into 'command' and 'args'
+        let trimmed = user_input.trim();
         if trimmed.is_empty() {
             continue
         }
-
         let mut it = trimmed.split_whitespace();
         let command = it.next().unwrap();
         let args: Vec<&str> = it.collect();
 
-
+        // command dispatch
         registry.dispatch(command, &args, &mut context);
     }
 }
